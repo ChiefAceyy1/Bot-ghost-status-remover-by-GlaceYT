ccd380f9935e287505030daa2e1fa1a53a044529
ChiefAceyy1 <chiefaceyy@gmail.com>
2024-10-19 15:52:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["👚 Fulfilling Orders", "💵 Cashing out and counting racks"];
+const statusMessages = ["👚 Fulfilling Orders", "💵 "];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
